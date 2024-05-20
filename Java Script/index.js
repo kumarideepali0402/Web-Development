312dee0fed93eb38450921fb7a0bc24c88dc1c93
kumarideepali0402 <154409860+kumarideepali0402@users.noreply.github.com>
2024-05-20 23:26:36 +0530

    done

---
+++ Java Script/index.js
@@ -0,0 +1,41 @@
+// HOF
+// const callback=(n)=>{
+//     return n**2;
+// }
+
+// function cube(){
+//     return callback(n) *2;
+// }
+// console.log(cube(callback,3));
+
+// setInterval
+
+// function greet(){
+//     console.log("hello");
+// }
+// setInterval(greet,1000);
+
+// setInterval(
+// () =>{
+// console.log("hello");
+// },
+// 1000);
+
+// setTimeout
+// setTimeout(
+// () =>{
+// console.log("hello");
+// },
+// 5000);
+
+// Functional Programming
+// Map filter reduce
+// Callback Hell
+
+// const num=[1,2,3,4]
+// const square=num.map((num)=> num*num);
+// console.log(square);
+
+// Filter
+const name=["Finland","Ireland","Pakistan"]
+const land=name.filter((country)=>)
